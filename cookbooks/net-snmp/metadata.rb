13eb54e41c55abbc5ac96fbfab17def315e4e9b9
Thomas Vincent <thomasvincent@gmail.com>
2026-04-11 16:36:30 -0700

    chore(cookbooks): bump nginx to 1.1.0, net-snmp to 2.1.0
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- cookbooks/net-snmp/metadata.rb
+++ cookbooks/net-snmp/metadata.rb
@@ -5,7 +5,7 @@ maintainer 'Thomas Vincent'
 maintainer_email 'thomasvincent@github.com'
 license 'Apache-2.0'
 description 'Installs and configures Net-SNMP with SNMPv3 support'
-version '2.0.0'
+version '2.1.0'
 chef_version '>= 18.0'
 
 # Supported platforms
